6b8952b0d4794a2a00e4241ccaffd156dd3eb98b
tonypeanut <tonypeanut93@gmail.com>
2024-02-02 22:41:28 -0700

    Ajustes para el paste, falta agregar al textarea el resultado filtrado

--- app.js
+++ app.js
@@ -30,7 +30,35 @@ function validarPaste(e){
     e.preventDefault();
     let paste = (e.clipboardData || window.clipboardData).getData("text");
 
-    console.log(typeof(paste))
+    pasteArray = paste.split("");
+
+    pasteCharCodes = [];
+    //Convertimos todos los elementos del sting paste a su charCode
+    pasteArray.forEach( (item,index) =>{pasteCharCodes.push(paste.charCodeAt(index));})
+
+    //Filtramos los charCodes que nos interesan
+    let flag = true;
+    while(flag){
+        flag = false;
+        pasteCharCodes.forEach( (item, index, object) => {
+            let code = parseInt(item);
+            if ((code >= 97 && code <=122) || code == 241 || code == 32){
+                
+            } else {
+                object.splice(index, 1);
+                flag = true;
+            }
+        });
+    }
+
+    //Convertimos los charcodes a char para forma el nuevo string
+    let nuevoArreglo = [];
+    pasteCharCodes.forEach( (item) => {
+        nuevoArreglo.push(String.fromCharCode(item));
+    })
+    
+    //Concatenamos el nuevo areglo y lo asignamos al pasteFiltrado
+    let pasteFiltrado = nuevoArreglo.join("");
 
 }
 
